ea2bf48ffc94df197572db92fcd715a16ba36856
Your Name <you@example.com>
2020-07-15 21:57:50 +0600

    Add projects files

---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex column"></q-page>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "PageIndex",
+
+  data() {
+    return {};
+  }
+});
+</script>
